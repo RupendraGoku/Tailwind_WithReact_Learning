ac987f1ec66b79aff6bd83cdea451e10d3e94de7
RupendraGoku <rkgupta8083@gmail.com>
2025-07-24 21:59:44 +0530

    double fixed

--- src/Components/ProductsInfo.jsx
+++ src/Components/ProductsInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Cards from './Cards'
-import { productCardData } from './cardData'
+import { productCardData } from './CardData'
 
 const ProductsInfo = () => {
   return (
