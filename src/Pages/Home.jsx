6f08b94e0eba57ce3127cde0437b858605e47fcd
RupendraGoku <rkgupta8083@gmail.com>
2025-07-24 22:01:58 +0530

    double fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../Components/Header';
 import Details from '../Components/Details';
 import CardInfo from '../Components/CardInfo';
 import Products from '../Components/Products';
-import ProductsInfo from '../Components/ProductsInfo.jsx';
+import ProductsInfo from '../Components/ProductsInfo';
 
 const Home = () => {
   return (
